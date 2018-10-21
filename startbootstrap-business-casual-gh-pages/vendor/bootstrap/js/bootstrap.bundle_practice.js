1f5b3c081acc1d2ac4c567e0cf0ecd845867c658
Oliver Schultz <oliverschultz@gmail.com>
2018-10-20 21:50:43 -0400

    commit - added new pic and sections

---
+++ startbootstrap-business-casual-gh-pages/vendor/bootstrap/js/bootstrap.bundle_practice.js
@@ -0,0 +1,8 @@
+var Selector = {
+  DATA_TOGGLE: '[data-toggle="dropdown"]',
+  FORM_CHILD: '.dropdown form',
+  MENU: '.dropdown-menu',
+  NAVBAR_NAV: '.navbar-nav',
+  VISIBLE_ITEMS: '.dropdown-menu .dropdown-item:not(.disabled):not(:disabled)'
+};
+
