74d6f742fd74dfc55c82a7dac5ceabfdcded244a
Oliver Schultz <oliverschultz@gmail.com>
2018-10-22 20:42:42 -0400

    commit

--- startbootstrap-business-casual-gh-pages/scrolling-nav.js
+++ startbootstrap-business-casual-gh-pages/scrolling-nav.js
@@ -1,29 +1,20 @@
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
+$(document).ready(function() {
+  $('#mainNav li a').click(function(e) {
+  	
+    alert('Clicked');
+    
+    e.preventDefault();
   });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+});
+$(document).ready(function() {
+  $('#mainNav li a').click(function(e) {
+  	
+  	var targetHref = $(this).attr('href');
+	  
+	$('html, body').animate({
+		scrollTop: $(targetHref).offset().top
+	}, 1000);
+    
+    e.preventDefault();
   });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-})(jQuery); // End of use strict
+});
