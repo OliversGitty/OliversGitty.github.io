186187724a83119397225a72fad6a4570e068ad1
ChrisC <ccary64@gmail.com>
2018-11-22 13:09:51 -0500

    remove old templating, cleanup folders

--- startbootstrap-business-casual-gh-pages/vendor/bootstrap/js/bootstrap.bundle_practice.js
+++
@@ -1,8 +0,0 @@
-var Selector = {
-  DATA_TOGGLE: '[data-toggle="dropdown"]',
-  FORM_CHILD: '.dropdown form',
-  MENU: '.dropdown-menu',
-  NAVBAR_NAV: '.navbar-nav',
-  VISIBLE_ITEMS: '.dropdown-menu .dropdown-item:not(.disabled):not(:disabled)'
-};
-
